b2e2e68de809b8dc48d6dcb3bdb76d32d152158c
thenullterminator <dazz2803@gmail.com>
2019-07-12 10:52:14 +0530

    Sort by drop down list created

--- src/components/ExpenseListFilter.js
+++ src/components/ExpenseListFilter.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import {setTextFilter} from '../Actions/filters';
+import {setTextFilter,sortByAmount,sortByDate} from '../Actions/filters';
 const ExpenseListFilter=(props)=>(
 
       <div>
-            <input type='text' value={props.filters.text} onChange={(e)=>{
-                  props.dispatch(setTextFilter(e.target.value));
-            }}/>
+            <input 
+                  type='text' 
+                  value={props.filters.text} 
+                  onChange={(e)=>{
+                        props.dispatch(setTextFilter(e.target.value));
+                  }}
+            />
+
+            <select 
+            value={props.filters.sortBy} 
+            onChange={(e)=>{
+
+                  if(e.target.value==='date'){
+                        props.dispatch(sortByDate());
+                  }else{
+                        props.dispatch(sortByAmount());
+                  }
+            }}
+            >
+                  <option value='date'>Date</option>
+                  <option value='amount'>Amount</option>
+            </select>
       </div>
 );
 
